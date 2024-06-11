77c146fb7d67dd95385835f6f52240e0f8990378
BatistaDevin <you@example.com>
2024-06-11 02:13:09 -0300

    v0.2

--- SistemaWeb/src/main/java/com/crudjspjava/dao/ConsultaDao.java
+++ SistemaWeb/src/main/java/com/crudjspjava/dao/ConsultaDao.java
@@ -16,7 +16,7 @@ public class ConsultaDao {
         Connection con = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:8080/cliente_db", "root", "");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3307/cliente_db", "root", "");
         } catch (Exception e) {
             System.out.println(e);
         }
